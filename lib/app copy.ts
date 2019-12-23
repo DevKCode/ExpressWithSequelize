b7d4ddfa2b14aa97ee3b0aa0056ccfea85c40e7c
dev <y2ksoft@gmail.com>
2019-12-23 18:08:51 +1300

    branch commit

---
+++ lib/app copy.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Routes } from './config/routes';
+
+class App {
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
+
+    constructor() {
+        this.app = express();
+        console.log('created');
+
+    }
+
+    private config(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    }
+}
+export default new App().app;
--- lib/controllers/NodeController.ts
+++ lib/controllers/NodeController.ts
@@ -21,7 +21,7 @@ export class NodeController {
     public show(req: Request, res: Response) {
         let nodeId: any = req.params.id;
         NodeModel.findByPk<NodeModel>(nodeId)
-            .then((node: NodeModel | null) => {
+            .then((nddodee: NodeModel | null) => {
 
             })
     }
